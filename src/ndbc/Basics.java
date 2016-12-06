602d0e39c33669dd93c36f0e0e8367af79066787
jpyams <jyamauchi@udallas.edu>
2016-12-06 14:24:09 -0600

    Fixed timer

--- src/ndbc/Basics.java
+++ src/ndbc/Basics.java
@@ -19,7 +19,7 @@ public class Basics extends Thread {
 			
 			timeleft = (int)((System.currentTimeMillis() - startTime) / 1000);
 			
-			if(timeleft > 5){
+			if(timeleft > 10){
 				panel.updateBotofPower();
 				startTime = System.currentTimeMillis();
 			}
